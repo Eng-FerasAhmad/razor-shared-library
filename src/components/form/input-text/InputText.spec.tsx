c5c36914719dfe0c1e9d7d7c013269500e477db3
Feras Ahmad <feras.ahmad@gmx.de>
2024-02-10 13:34:10 +0100

    change test concept

--- src/components/form/input-text/InputText.spec.tsx
+++ src/components/form/input-text/InputText.spec.tsx
@@ -1,94 +1,12 @@
-import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import { InputText } from './InputText';
-import { color } from 'src/shared/color';
 import { render, screen } from 'src/test/test-utils';
 
 describe('<InputText />', () => {
     test('should render the component', () => {
         render(
-            <InputText label="label" handleChange={jest.fn()} value="value" />
+            <InputText label="label" value="value" />
         );
 
         expect(screen.getByTestId('input-text-container')).toBeInTheDocument();
-        expect(screen.getByTestId('input-text')).toBeInTheDocument();
-        expect(screen.getByTestId('input-label')).toBeInTheDocument();
-    });
-
-    test('should render the error input', () => {
-        render(
-            <InputText
-                label="label"
-                handleChange={jest.fn()}
-                value="value"
-                errorLabel="error"
-            />
-        );
-
-        expect(screen.getByTestId('input-text-container')).toBeInTheDocument();
-        expect(screen.getByTestId('input-error')).toBeInTheDocument();
-    });
-
-    test('should handle clear icon', async () => {
-        const handleChange = jest.fn();
-        render(
-            <InputText
-                label="label"
-                handleChange={handleChange}
-                value="value"
-            />
-        );
-
-        expect(screen.getByTestId('input-text-container')).toBeInTheDocument();
-
-        const input = screen.getByTestId('input-text');
-        await act(async () => {
-            await userEvent.click(input);
-        });
-
-        const clear = screen.getByTestId('input-clear');
-        expect(clear).toBeInTheDocument();
-        await act(async () => {
-            await userEvent.click(clear);
-        });
-
-        expect(handleChange).toHaveBeenCalledWith('');
-    });
-
-    test('should handle change value', async () => {
-        const handleChange = jest.fn();
-        render(
-            <InputText
-                label="label"
-                handleChange={handleChange}
-                value="value"
-            />
-        );
-
-        expect(screen.getByTestId('input-text-container')).toBeInTheDocument();
-
-        const input = screen.getByTestId('input-text');
-        await act(async () => {
-            await userEvent.clear(input);
-        });
-
-        expect(handleChange).toHaveBeenCalledWith('');
-    });
-
-    test('should render disabled input', () => {
-        const handleChange = jest.fn();
-        render(
-            <InputText
-                label="label"
-                handleChange={handleChange}
-                value="value"
-                disabled={true}
-            />
-        );
-
-        expect(screen.getByTestId('input-text')).toBeInTheDocument();
-        expect(screen.getByTestId('input-text')).toHaveStyle(
-            `background-color:${color.disabled}`
-        );
     });
 });
