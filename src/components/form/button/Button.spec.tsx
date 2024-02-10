47d57dc779dab610b0dbc8acdcedff1fe2231333
Feras Ahmad <feras.ahmad@gmx.de>
2024-02-10 13:43:52 +0100

    fix linter

--- src/components/form/button/Button.spec.tsx
+++ src/components/form/button/Button.spec.tsx
@@ -1,11 +1,9 @@
-import { render, screen } from 'src/test/test-utils';
 import { ButtonCustom } from './Button';
+import { render, screen } from 'src/test/test-utils';
 
 describe('<ButtonCustom />', () => {
     test('should render the primary button', () => {
-        render(
-            <ButtonCustom label="button" color="primary" />
-        );
+        render(<ButtonCustom label="button" color="primary" />);
 
         const button = screen.getByRole('button', { name: 'button' });
         expect(button).toBeInTheDocument();
--- src/components/form/input-text/InputText.spec.tsx
+++ src/components/form/input-text/InputText.spec.tsx
@@ -3,9 +3,7 @@ import { render, screen } from 'src/test/test-utils';
 
 describe('<InputText />', () => {
     test('should render the component', () => {
-        render(
-            <InputText label="label" value="value" />
-        );
+        render(<InputText label="label" value="value" />);
 
         expect(screen.getByTestId('input-text-container')).toBeInTheDocument();
     });
