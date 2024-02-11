f7d330097b0de182bd0a5a384102050db208223f
Feras Ahmad <55232766+Eng-FerasAhmad@users.noreply.github.com>
2024-02-11 17:53:28 +0100

    improve input size and color (#14)

--- src/components/form/input-text/InputText.tsx
+++ src/components/form/input-text/InputText.tsx
@@ -1,10 +1,15 @@
 import TextField, { TextFieldProps } from '@mui/material/TextField';
 import { Template } from 'src/components/_template/Template';
+import { color } from 'src/shared/color';
 
 export function InputText({ ...props }: TextFieldProps): JSX.Element {
     return (
         <Template>
-            <TextField {...props} data-testid="input-text-container" />
+            <TextField
+                {...props}
+                data-testid="input-text-container"
+                sx={{ backgroundColor: color.light, width: '100%' }}
+            />
         </Template>
     );
 }
