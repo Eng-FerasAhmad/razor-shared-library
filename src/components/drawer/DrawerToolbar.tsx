71b7ece033aaa7affd5d18ab7d561f74f2a2412a
Feras Ahmad <feras.ahmad@gmx.de>
2024-03-10 22:35:24 +0100

    change drawer toolbar name

--- src/components/drawer/DrawerToolbar.tsx
+++ src/components/drawer/DrawerToolbar.tsx
@@ -6,13 +6,15 @@ import { DrawerAppBar } from 'src/components/drawer/styles';
 import { fontSize } from 'src/shared/fonts';
 
 interface Props {
+    appTitle: string;
     header: ReactNode;
     open: boolean;
     headerIcon?: ReactNode;
     appClickHandler: () => void;
 }
 
-export default function AppToolbar({
+export default function DrawerToolbar({
+    appTitle,
     header,
     open,
     headerIcon,
@@ -33,11 +35,12 @@ export default function AppToolbar({
                 <Box
                     sx={{
                         fontSize: fontSize.font18,
+                        width: '100%',
                         mr: 2,
                         ...(open && { display: 'none' }),
                     }}
                 >
-                    Razor Admin
+                    {appTitle}
                 </Box>
                 {header}
             </Toolbar>
