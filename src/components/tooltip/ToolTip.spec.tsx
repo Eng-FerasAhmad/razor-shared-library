5b3d2168af78b3fbe78cfcf17149e0a766523b7a
Feras Ahmad <feras.ahmad@gmx.de>
2024-04-14 20:48:30 +0200

    fix lint

--- src/components/tooltip/ToolTip.spec.tsx
+++ src/components/tooltip/ToolTip.spec.tsx
@@ -1,11 +1,10 @@
-import { render, screen } from 'src/test/test-utils';
 import { ToolTipCustom } from './ToolTip';
+import { render, screen } from 'src/test/test-utils';
 
 describe('<ToolTip />', () => {
     test('should render the component', () => {
         render(
-            <ToolTipCustom title='tooltip'
-            >
+            <ToolTipCustom title="tooltip">
                 <div>text</div>
             </ToolTipCustom>
         );
--- src/components/tooltip/ToolTip.tsx
+++ src/components/tooltip/ToolTip.tsx
@@ -1,16 +1,11 @@
-
-
 import Tooltip from '@mui/material/Tooltip';
 
 interface Props {
     title: string;
-    children: JSX.Element,
+    children: JSX.Element;
 }
 
-export function ToolTipCustom({
-    title,
-    children,
-}: Props): JSX.Element {
+export function ToolTipCustom({ title, children }: Props): JSX.Element {
     return (
         <Tooltip title={title} data-testid="tooltip">
             {children}
