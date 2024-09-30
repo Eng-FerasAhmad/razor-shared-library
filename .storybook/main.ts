c136b0a0201cf0c3352b2ae93320f18f6cbe9e8d
Feras Ahmad <feras.ahmad@ippen-digital.de>
2024-09-30 22:46:47 +0200

    add new docs to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    stories: [
+        '../src/Welcome.stories.mdx',
+        '../src/**/*.mdx',
+        '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    ],
     addons: [
         '@storybook/addon-onboarding',
         '@storybook/addon-links',
@@ -12,5 +16,10 @@ const config: StorybookConfig = {
         name: '@storybook/react-vite',
         options: {},
     },
+    viteFinal: async (config) => {
+        config.base = '/razor-shared-library/';
+        return config;
+    },
 };
+
 export default config;
