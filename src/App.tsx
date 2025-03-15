7b3aea496ac21e1f23b3cf581e687aace0384a86
Feras Ahmad <feras.ahmad@gmx.de>
2025-03-15 17:36:23 +0100

    Add story for the studio component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
 import { ReactElement } from 'react';
 
-import { Studio } from 'components/factory/studio/Studio';
-import { generateImageUrls } from 'components/factory/studio/fixed-grid-view/utils';
-
 export default function App(): ReactElement {
-    const images = generateImageUrls(100);
-    return (
-        <div style={{ marginTop: '50px' }}>
-            <Studio
-                media={images}
-                noMediaTitle={'No Media'}
-                fullscreen={true}
-                infoButton={true}
-                buttonMore={'Show more'}
-                buttonReset={'Reset'}
-            />
-        </div>
-    );
+    return <div style={{ marginTop: '50px' }}></div>;
 }
---
+++ src/components/factory/studio/Studio.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { generateImageUrls } from 'components/factory/studio/fixed-grid-view/utils';
+
+import { Studio } from './Studio';
+
+const images = generateImageUrls(100);
+
+const meta: Meta<typeof Studio> = {
+    title: 'Factory/Studio',
+    component: Studio,
+    argTypes: {
+        fullscreen: {
+            control: 'boolean',
+        },
+        infoButton: {
+            control: 'boolean',
+        },
+        buttonMore: {
+            control: 'text',
+        },
+        buttonReset: {
+            control: 'text',
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Studio>;
+
+export const Default: Story = {
+    render: (args) => (
+        <div style={{ marginTop: '50px' }}>
+            <Studio {...args} />
+        </div>
+    ),
+    args: {
+        media: images,
+        noMediaTitle: 'No Media',
+        fullscreen: true,
+        infoButton: true,
+        buttonMore: 'Show more',
+        buttonReset: 'Reset',
+    },
+};
